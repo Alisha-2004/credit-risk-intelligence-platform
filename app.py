4b7f7ead08bfa3f3b45775d563d15acf2063558e
Alisha Mary Shibu <alishamaryshibu@gmail.com>
2026-06-01 11:37:07 +0530

    Fix sidebar navigation

--- app.py
+++ app.py
@@ -175,14 +175,13 @@ st.sidebar.info(
 page = st.sidebar.radio(
     "Navigation",
     [
-        "Home",
-        "Risk Prediction",
-        "Explainability",
-        "Talk To Data"
+        "🏠 Home",
+        "📈 Risk Prediction",
+        "🔍 Explainability",
+        "💬 Talk To Data"
     ],
     label_visibility="collapsed"
 )
-
 # =====================================
 
 # HOME PAGE
@@ -191,7 +190,7 @@ page = st.sidebar.radio(
 st.success(
     "Welcome to the Credit Risk Intelligence Platform. Explore risk prediction, explainability, and AI-powered analytics."
 )
-if page == "🏠 Home":
+if page == "Home":
 
 
     st.markdown(
@@ -282,7 +281,7 @@ if page == "🏠 Home":
 
 # =====================================
 
-elif page == "📈 Risk Prediction":
+elif page == "Risk Prediction":
 
 
     st.markdown(
@@ -345,7 +344,7 @@ elif page == "📈 Risk Prediction":
 
 # =====================================
 
-elif page == "🔍 Explainability":
+elif page == "Explainability":
 
 
     st.markdown(
@@ -371,7 +370,7 @@ elif page == "🔍 Explainability":
 
 # =====================================
 
-elif page == "💬 Talk To Data":
+elif page == "Talk To Data":
 
 
     st.markdown(
